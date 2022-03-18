e5c3237c8d7fbc4cfef0fdcf0adcbf77aacf5658
tassaron <rutachigal@gmail.com>
2022-03-18 12:46:43 -0400

    minimize bundle in production, create source map in dev

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,41 @@ const webpack = require("webpack");
 const path = require("path");
 const TerserPlugin =  require('terser-webpack-plugin')
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: ['./main.js'],
-    module: {
-        rules: [
-            /*{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },*/
-            {
-                test: /\.(ts|js)?/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    devtool: 'source-map',
-    output: {
-      path: path.resolve(__dirname),
-      filename: 'bundle.js',
-    },
-    plugins:[new TerserPlugin()]
+module.exports = (env, argv) => {
+    const config = {
+        context: path.resolve(__dirname, 'src'),
+        entry: ['./main.js'],
+        module: {
+            rules: [
+                /*{
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                },*/
+                {
+                    test: /\.(ts|js)?/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        output: {
+            path: path.resolve(__dirname),
+            filename: 'bundle.js',
+        },
+    };
+
+    if (argv.mode == "production") {
+        const TerserPlugin = require('terser-webpack-plugin');
+        config.optimization = {
+            minimizer: [new TerserPlugin({})],
+        }
+    } else if (argv.mode == "development") {
+        config.devtool = 'source-map';
+    }
+
+    return config;
 }
