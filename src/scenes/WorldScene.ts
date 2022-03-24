2bead73cae4c9db981def4db17dc4066401ecc16
tassaron <rutachigal@gmail.com>
2022-03-24 17:54:12 -0400

    World scene draws some random circles

--- src/scenes/WorldScene.ts
+++ src/scenes/WorldScene.ts
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import { logger } from "../logger";
 import IGame from "../interfaces/IGame";
 import BaseScene from "./BaseScene";
+import DrawnEllipse from "../entities/DrawnEllipse";
 
 
 export default class WorldScene extends BaseScene {
@@ -9,12 +10,27 @@ export default class WorldScene extends BaseScene {
 
     constructor(game: IGame) {
         super(game);
-        game.containers.root.removeChildren();
-        
+        const ball = () => new DrawnEllipse(game, 30, 30, 0x666666, null);
+        this.actors.balls = [
+            ball(), ball(), ball(), ball()
+        ];
+        for (let ball of this.actors.balls) {
+            ball.x = Math.random() * this.game.width;
+            ball.y = Math.random() * this.game.height;
+        }
         logger.info("Created World scene");
     }
 
     mount(container: PIXI.Container) {
         this.game.prevScene.unmount(container);
+        for (let ball of this.actors.balls) {
+            container.addChild(ball);
+        }
+    }
+
+    unmount(container: PIXI.Container) {
+        for (let ball of this.actors.balls) {
+            container.removeChild(ball);
+        }
     }
 }
