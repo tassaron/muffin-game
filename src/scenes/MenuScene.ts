c2054f439a6b696f95612a418fc85e4c74883d6e
tassaron <rutachigal@gmail.com>
2022-03-24 09:45:19 -0400

    stub WorldScene, change to it when clicking button

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -1,8 +1,8 @@
-//import { WorldScene } from "./world.js";
 import { logger } from "../logger";
 import IGame from "../interfaces/IGame";
 import BaseScene from "./BaseScene";
 import Button from "../entities/Button";
+import WorldScene from "./WorldScene";
 
 export default class MenuScene extends BaseScene {
     actors: any = {};
@@ -10,20 +10,33 @@ export default class MenuScene extends BaseScene {
     constructor(game: IGame) {
         super(game);
         game.containers.root.removeChildren();
-        this.actors.explosion = game.sprites.explosion;
         this.actors.button = new Button(game, 200, 50, 0x000000, 0xffffff, "Start Game");
-        logger.info("Created Menu scene");
-        game.containers.root.addChild(this.actors.explosion);
         game.containers.root.addChild(this.actors.button);
         this.actors.button.x = game.width / 2;
         this.actors.button.y = game.height / 2;
-
+        
         // Hook events to buttons
         this.actors.button.interactive = true;
-        this.actors.button.click = (e: Event) => logger.info(`${e.type} at ${e.target}`);
+        this.actors.button.click = (_: Event) => this.clickStart();
+        
+        logger.info("Created Menu scene");
     }
 
     tick(delta: number, keyboard: any) {
+        if (this.actors.explosion === undefined) return
         this.actors.explosion.tick(delta);
+        if (this.actors.explosion.loops > 0) {
+            this.game.changeScene(new WorldScene(this.game));
+        }
+    }
+
+    clickStart() {
+        if (this.actors.explosion === undefined) {
+            this.actors.explosion = this.game.sprites.explosion;
+            this.actors.explosion.x = this.game.width / 2;
+            this.actors.explosion.y = this.game.height / 2;
+            this.game.containers.root.addChild(this.actors.explosion);
+            this.game.containers.root.removeChild(this.actors.button);
+        }
     }
 }
---
+++ src/scenes/WorldScene.ts
@@ -0,0 +1,15 @@
+import { logger } from "../logger";
+import IGame from "../interfaces/IGame";
+import BaseScene from "./BaseScene";
+
+
+export default class WorldScene extends BaseScene {
+    actors: any = {};
+
+    constructor(game: IGame) {
+        super(game);
+        game.containers.root.removeChildren();
+        
+        logger.info("Created World scene");
+    }
+}
