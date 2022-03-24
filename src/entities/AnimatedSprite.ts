5471fc65ad9d79a5d3be802e230d267beaa9cad4
tassaron <rutachigal@gmail.com>
2022-03-24 09:44:17 -0400

    remove double-underscores from most attrs of AnimatedSprite

--- src/entities/AnimatedSprite.ts
+++ src/entities/AnimatedSprite.ts
@@ -14,32 +14,32 @@ function setFrame(texture: PIXI.Texture, w: number, h: number, number: number) {
 export default class AnimatedSprite extends PIXI.Sprite implements IAnimatedSprite {
     __width: number;
     __height: number;
-    __numFrames: number;
-    __currFrame: number;
-    __delayInFrames: number;
-    __loops: number;
-    __animTimer: number;
+    numFrames: number;
+    currFrame: number;
+    delayInFrames: number;
+    animTimer: number;
+    loops: number;
 
     constructor(texture: PIXI.Texture, width: number, height: number, numFrames: number, delayInFrames: number) {
         setFrame(texture, width, height, 0);
         super(texture);
         this.__width = width;
         this.__height = height;
-        this.__numFrames = numFrames;
-        this.__currFrame = 0;
-        this.__delayInFrames = delayInFrames;
-        this.__animTimer = 0.0;
-        this.__loops = 0;
+        this.numFrames = numFrames;
+        this.currFrame = 0;
+        this.delayInFrames = delayInFrames;
+        this.animTimer = 0.0;
+        this.loops = 0;
     }
 
     tick(delta: number) {
-        const newFrame = Math.floor(this.__animTimer / this.__delayInFrames);
-        if (newFrame != this.__currFrame) {
-            this.__currFrame = newFrame;
-            setFrame(this.texture, this.__width, this.__height, this.__currFrame);
+        const newFrame = Math.floor(this.animTimer / this.delayInFrames);
+        if (newFrame != this.currFrame) {
+            this.currFrame = newFrame;
+            setFrame(this.texture, this.__width, this.__height, this.currFrame);
         }
-        this.__animTimer += delta;
-        if (this.__animTimer > this.__numFrames * this.__delayInFrames) {this.__animTimer = 0.0; this.__loops++;}
+        this.animTimer += delta;
+        if (this.animTimer > this.numFrames * this.delayInFrames) {this.animTimer = 0.0; this.loops++;}
         this.texture.update();
     }
 }
--- src/interfaces/IAnimatedSprite.ts
+++ src/interfaces/IAnimatedSprite.ts
@@ -1,10 +1,10 @@
 export default interface IAnimatedSprite {
     __width: number,
     __height: number,
-    __numFrames: number,
-    __currFrame: number,
-    __delayInFrames: number,
-    __loops: number,
-    __animTimer: number,
+    numFrames: number,
+    currFrame: number,
+    delayInFrames: number,
+    loops: number,
+    animTimer: number,
     tick(delta: number): void,
 }
