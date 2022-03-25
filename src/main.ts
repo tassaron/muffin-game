886b8637176e134a02556046e730645b8b73873a
tassaron <rutachigal@gmail.com>
2022-03-25 18:35:14 -0400

    stop ticking the loadingtext after preload is complete

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const PREFIX = "assets/";
 const app = new PIXI.Application({
     width: 800, height: 600, backgroundColor: 0xbcbcf2, useContextAlpha: false, resolution: window.devicePixelRatio || 1,
 });
-function loadingText(app: PIXI.Application) {
+function loadingText(app: PIXI.Application): (delta: number) => void {
     const skewStyle = new PIXI.TextStyle({
         fontFamily: 'var(--arcade-font)',
         dropShadow: true,
@@ -38,7 +38,8 @@ function loadingText(app: PIXI.Application) {
     skewText.anchor.set(0.5, 0.5);
     skewText.x = 400;
     skewText.y = 300;
-    app.ticker.add((delta) => {
+
+    const tickText = (delta: number) => {
         if (skewText.rotation > 0) {
             skewText.rotation -= delta / 30;
             skewText.skew.x -= Math.log(delta) /30;
@@ -46,10 +47,12 @@ function loadingText(app: PIXI.Application) {
             skewText.rotation += Math.log(delta) / 30;
             skewText.skew.x += delta / 30;
         }
-    })
-    return skewText;
+    };
+    app.stage.addChild(skewText);
+    return tickText;
 }
-app.stage.addChild(loadingText(app));
+const textTicker = loadingText(app);
+app.ticker.add(textTicker);
 
 
 const gameDiv: HTMLElement | null = document.getElementById("game");
@@ -70,6 +73,7 @@ for (let texture of textures) {
 loader
     .load((loader, resources) => after_preload(loader, resources, sprites))
     .onComplete.add(() => {
+        app.ticker.remove(textTicker);
         const game = new Game(app, sprites, keyboard);
         const pauseButton: HTMLElement | null = document.getElementById("pause_button");
         pauseButton && pauseButton.addEventListener('click', () => game.pause(keyboard), false);
