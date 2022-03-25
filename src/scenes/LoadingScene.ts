91a3c97ee215493565322a47476d4b03e033b6d7
tassaron <rutachigal@gmail.com>
2022-03-25 00:25:25 -0400

    add log message to LoadingScene for consistency

--- src/scenes/LoadingScene.ts
+++ src/scenes/LoadingScene.ts
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-//import { logger } from "../logger";
+import { logger } from "../logger";
 import IGame from "../interfaces/IGame";
 import BaseScene from "./BaseScene";
 
@@ -8,7 +8,8 @@ export default class LoadingScene extends BaseScene {
     actors: any = {};
 
     constructor(game: IGame) {
-        super(game);        
+        super(game);
+        logger.info("Created Loading scene");
     }
 
     unmount(container: PIXI.Container) {
