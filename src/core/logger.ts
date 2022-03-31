13fffaae7e32afd10b687e5954a4ec67a91be863
tassaron <rutachigal@gmail.com>
2022-03-31 18:29:01 -0400

    logger namespaces

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,25 +1,42 @@
-class Logger {
-    LOGLEVEL = 0;
+export default class Logger {
+    level = {
+        spam: -1,
+        verbose: 0,
+        debug: 1,
+        info: 2,
+        error: 3,
+        disabled: 5,
+    };
+    minimum = 0;
+    namespace = "";
+
+    constructor(namespace = "") {
+        this.namespace = namespace;
+    }
+
+    formatMsg(string: string) {
+        return `${this.namespace}${this.namespace ? ": " : ""}${string}`
+    }
 
     spam(msg: string) {
-        this.LOGLEVEL < 0 ? console.log(msg) : null
+        this.minimum < 0 ? console.log(this.formatMsg(msg)) : null;
     }
 
     verbose(msg: string) {
-        this.LOGLEVEL < 1 ? console.log(msg) : null
+        this.minimum < 1 ? console.log(this.formatMsg(msg)) : null;
     }
 
     debug(msg: string) {
-        this.LOGLEVEL < 2 ? console.log(msg) : null
+        this.minimum < 2 ? console.log(this.formatMsg(msg)) : null;
     }
     
     info(msg: string) {
-        this.LOGLEVEL < 3 ? console.info(msg) : null
+        this.minimum < 3 ? console.info(this.formatMsg(msg)) : null;
     }
     
     error(msg: string) {
-        this.LOGLEVEL < 4 ? console.error(msg) : null
+        this.minimum < 4 ? console.error(this.formatMsg(msg)) : null;
     }
 }
 
-export const logger = new Logger();
+export const logger = new Logger("muffin-game");
